b500a28e7ee71df276669da5a5a97e6e50e32e4a
Ododah David <95848067+Ododahdavid@users.noreply.github.com>
2024-09-03 22:21:13 +0100

    made and endpoint to fetch courses on students search page.

--- NodeServer/routes/studentRoutes/studentRoutes.js
+++ NodeServer/routes/studentRoutes/studentRoutes.js
@@ -3,6 +3,7 @@ import jwt from 'jsonwebtoken';
 import Student from '../../models/studentModels/studentModel.js'
 import sendEmail from "../../utils/email.js";
 import crypto from "crypto"
+import Course from '../../models/tutorModels/coursesModel.js';
 
 const router = express.Router();
 
@@ -17,7 +18,7 @@ const generateSignToken = (_id, email) => {
   };
 
 
-  
+  // Route to Create / register a new student into the system
 router.post('/new/student', async (req, res, next) => {
     const { firstname, lastname, email, password } = req.body;
   
@@ -75,6 +76,31 @@ router.post("/login/student", async (req, res, next) => {
     }
     catch (err){
       next(err) 
+    }
+  })
+
+
+
+  // endpoint to get top 20 recommended courses fromtutor in the student search page
+  router.get("/student/recommended/courses", async(req, res, next) => {
+    try{
+      const offset = parseInt(req.query.offset) || 0;  // Default to offset 0... the offset represesnt the amount of courses allready fetched from the database. 
+      const limit = parseInt(req.query.limit) || 20;  // Default to 20 courses per request
+      // code to fetch the first 20 recommended courses from tutors on students search page.
+      const recommendedCourses = await Course.find().skip(offset).limit(limit)
+      // the line above, simply skips the offset and fetches the next set of data in this case courses. and maintains a limit of 20 courses to be fetched
+      // If no courses are found, send a 404 response
+      if (recommendedCourses.length === 0) {
+        return res.status(404).send({ message: "No recommended courses found" });
+      }
+  
+      // Send the recommended courses as a response
+      res.status(200).send(recommendedCourses);
+    }
+    catch(err){
+       // Log the error and send a 500 response with a generic error message
+    console.error(err);
+    res.status(500).send({ message: "An error occurred while fetching recommended courses" });
     }
   })
 
